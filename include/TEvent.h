fa33f794bffe7cae67f9816ca6971d2e8d823581
Saurabhsawant000 <saurabhsawant000@gmail.com>
2026-03-05 10:47:31 -0400

    Upstream detectors were removed

--- include/TEvent.h
+++ include/TEvent.h
@@ -13,39 +13,6 @@ class TEvent : public TObject {
 		TEvent(const TEvent &); //!
 		virtual ~TEvent() {} //!
 		
-		std::vector<Double_t>	Qdet_tlP;
-		std::vector<Double_t>	Qdet_blP;
-		std::vector<Double_t>	Ex_tlP;
-		std::vector<Double_t>	Ex_blP;
-		std::vector<Double_t>	YdCsIETot_tlP;
-		std::vector<Double_t>	YdCsIETot_blP;
-		std::vector<Double_t>	EB_det_tlP;
-		std::vector<Double_t>	EB_det_blP;
-		std::vector<Double_t>	PB_det_tlP;
-		std::vector<Double_t>	PB_det_blP;
-		
-		//Double_t fQsd;
-		
-		std::vector<Double_t> 	fEYd_tlP;
-		std::vector<Double_t>	fEYd_blP;
-		std::vector<Double_t>	fCCsI_blP; 
-		std::vector<Double_t>	fCCsI_tlP;     
-		std::vector<Double_t>	fECsI_blP; 
-		std::vector<Double_t>	fECsI_tlP;
-		
-		std::vector<Double_t>	fEb_blP;
-		std::vector<Double_t>	fPb_blP;
-		std::vector<Double_t>	fPby_blP;
-		std::vector<Double_t>	fPbxcm_blP;
-		std::vector<Double_t>	fEb_tlP;
-		std::vector<Double_t>	fPb_tlP;
-		std::vector<Double_t>	fPby_tlP;
-		std::vector<Double_t>	fPbxcm_tlP;
-		std::vector<Double_t>	fThetacm_blP;
-		std::vector<Double_t>	fThetacm_tlP;
-		std::vector<Double_t>	fTheta_blP;
-		std::vector<Double_t>	fTheta_tlP;
-				
 		Double_t fEBAC;
 		Double_t fmA;
 		Double_t fma;
@@ -55,17 +22,109 @@ class TEvent : public TObject {
 		Double_t fEBeam;
 		Double_t fbetaCM;
 		Double_t fgammaCM;
-		Double_t fPA;
-	  	/*Double_t fPBeam;
-	  	Double_t fPResid;
-	  	Double_t fA;
-	  	Double_t fB;
-	  	Double_t fC;
-	   	*/
-			    
+		Double_t fPA;		    
 		Double_t fLP;	// Light particle energy
 		Double_t fHP;	// Heavy particle energy
 		Double_t fEB;	// Measured heavy particle energy
+
+		std::vector<Double_t> Qdet1_tlP;
+		std::vector<Double_t> Ex1_tlP;
+		std::vector<Double_t> YdCsI1ETot_tlP;
+		std::vector<Double_t> EB1_det_tlP;
+		std::vector<Double_t> PB1_det_tlP;
+		std::vector<Double_t> fEYd1_tlP;
+		std::vector<Double_t> fCCsI1_tlP;
+		std::vector<Double_t> fECsI1_tlP;
+		std::vector<Double_t> fEb1_tlP;
+		std::vector<Double_t> fPb1_tlP;
+  		std::vector<Double_t> fPby1_tlP;
+  		std::vector<Double_t> fPbxcm1_tlP;
+		std::vector<Double_t> fThetacm1_tlP;
+		std::vector<Double_t> fTheta1_tlP;
+
+		std::vector<Double_t> Qdet2_tlP;
+		std::vector<Double_t> Ex2_tlP;
+		std::vector<Double_t> YdCsI2ETot_tlP;
+		std::vector<Double_t> EB2_det_tlP;
+		std::vector<Double_t> PB2_det_tlP; //Calculated heavy particle momentum
+		std::vector<Double_t> fEYd2_tlP;
+		std::vector<Double_t> fCCsI2_tlP;
+		std::vector<Double_t> fECsI2_tlP;
+		std::vector<Double_t> fEb2_tlP;
+		std::vector<Double_t> fPb2_tlP;
+  		std::vector<Double_t> fPby2_tlP;
+  		std::vector<Double_t> fPbxcm2_tlP;
+		std::vector<Double_t> fThetacm2_tlP;
+		std::vector<Double_t> fTheta2_tlP;
+
+		std::vector<Double_t> fPBeam_tlP;
+  		std::vector<Double_t> fPResid_tlP;
+  		std::vector<Double_t> fA_tlP;
+  		std::vector<Double_t> fB_tlP;
+  		std::vector<Double_t> fC_tlP;
+		std::vector<Double_t> fEB_tlP;
+		std::vector<Double_t>TSdThetaCM_tlP;
+		std::vector<Double_t>TSdETot_tlP;
+
+		std::vector<Double_t> Qdet1_blP;
+		std::vector<Double_t> Ex1_blP;
+		std::vector<Double_t> YdCsI1ETot_blP;
+		std::vector<Double_t> EB1_det_blP; //Calculated heavy particle energy
+		std::vector<Double_t> PB1_det_blP; //Calculated heavy particle momentum
+		std::vector<Double_t> fEYd1_blP;
+		std::vector<Double_t> fCCsI1_blP; 
+		std::vector<Double_t> fECsI1_blP; 
+		std::vector<Double_t> fEb1_blP;
+  		std::vector<Double_t> fPb1_blP;
+  		std::vector<Double_t> fPby1_blP;
+  		std::vector<Double_t> fPbxcm1_blP;
+		std::vector<Double_t> fThetacm1_blP;
+		std::vector<Double_t> fTheta1_blP;
+ 
+		std::vector<Double_t> Qdet2_blP;
+		std::vector<Double_t> Ex2_blP;
+		std::vector<Double_t> YdCsI2ETot_blP;
+		std::vector<Double_t> EB2_det_blP; //Calculated heavy particle energy
+		std::vector<Double_t> PB2_det_blP; //Calculated heavy particle momentum
+		std::vector<Double_t> fEYd2_blP;
+		std::vector<Double_t> fCCsI2_blP; 
+		std::vector<Double_t> fECsI2_blP; 
+		std::vector<Double_t> fEb2_blP;
+  		std::vector<Double_t> fPb2_blP;
+  		std::vector<Double_t> fPby2_blP;
+  		std::vector<Double_t> fPbxcm2_blP;
+		std::vector<Double_t> fThetacm2_blP;
+		std::vector<Double_t> fTheta2_blP;
+ 
+		std::vector<Double_t> fPBeam_blP;
+  		std::vector<Double_t> fPResid_blP;
+  		std::vector<Double_t> fA_blP;
+  		std::vector<Double_t> fB_blP;
+  		std::vector<Double_t> fC_blP;
+		std::vector<Double_t> fEB_blP;
+		std::vector<Double_t> TSdThetaCM_blP;
+		std::vector<Double_t> TSdETot_blP;
+				
+		Double_t fEbU;
+  		Double_t fPbU;
+  		Double_t fPbUy;
+  		Double_t fPbUxcm;
+		Double_t fEBU; //Calculated heavy particle energy
+		Double_t fPBU; //Calculated heavy particle momentum
+		Double_t fQvU;
+		Double_t fExU;
+		Double_t fThetaDU;
+		Double_t fThetacmU;
+		 
+		Double_t fEbUSd;
+  		Double_t fPbUSd;
+  		Double_t fPbUSdy;
+  		Double_t fPbUSdxcm;
+		Double_t fEBUSd; //Calculated heavy particle energy
+		Double_t fPBUSd; //Calculated heavy particle momentum
+		Double_t fQvUSd;
+		Double_t fThetacmUSd;
+		Double_t fThetaDUSd;
 		
 		void Clear();	//! Clear values
 		
--- src/TEvent.cxx
+++ src/TEvent.cxx
@@ -17,37 +17,6 @@ TEvent::TEvent(const TEvent &evt) : TObject(evt) //
 
 void TEvent::Clear(){
 
-	Qdet_tlP.clear();
-	Qdet_blP.clear();
-	Ex_tlP.clear();
-	Ex_blP.clear();
-	YdCsIETot_tlP.clear();
-	YdCsIETot_blP.clear();
-	EB_det_tlP.clear();
-	EB_det_blP.clear(); //Calculated heavy particle energy
-	PB_det_tlP.clear(); //Calculated heavy particle momentum
-	PB_det_blP.clear(); //Calculated heavy particle momentum
-	
-	fEYd_tlP.clear();
-	fEYd_blP.clear();
-	fCCsI_blP.clear(); 
-	fCCsI_tlP.clear();     
-	fECsI_blP.clear(); 
-	fECsI_tlP.clear();
-	
-	fEb_blP.clear();
-  	fPb_blP.clear();
-  	fPby_blP.clear();
-  	fPbxcm_blP.clear();
-	fEb_tlP.clear();
-  	fPb_tlP.clear();
-  	fPby_tlP.clear();
-  	fPbxcm_tlP.clear();
-	fThetacm_blP.clear();
-	fThetacm_tlP.clear();
-	fTheta_blP.clear();
-	fTheta_tlP.clear();
-	
 	fEBAC = sqrt(-1.);
 	fmA = sqrt(-1.);
 	fma = sqrt(-1.);
@@ -58,15 +27,113 @@ void TEvent::Clear(){
 	fbetaCM = sqrt(-1.);
 	fgammaCM = sqrt(-1.);
 	fPA = sqrt(-1.);
-  	/*fPBeam = sqrt(-1.);
-  	fPResid = sqrt(-1.);
-  	fA = sqrt(-1.);
-  	fB = sqrt(-1.);
-  	fC = sqrt(-1.);*/
-
 	fLP=0; //Light particle energy
 	fHP=0; //Heavy particle energy
 	fEB=0; //Measured heavy particle energy
+	
+	Qdet1_tlP.clear();
+	Ex1_tlP.clear();
+	YdCsI1ETot_tlP.clear();
+	EB1_det_tlP.clear();
+	PB1_det_tlP.clear(); //Calculated heavy particle momentum
+	fEYd1_tlP.clear();
+	fCCsI1_tlP.clear();
+	fECsI1_tlP.clear();
+	fEb1_tlP.clear();
+	fPb1_tlP.clear();
+  	fPby1_tlP.clear();
+  	fPbxcm1_tlP.clear();
+	fThetacm1_tlP.clear();
+	fTheta1_tlP.clear();
+
+	Qdet2_tlP.clear();
+	Ex2_tlP.clear();
+	YdCsI2ETot_tlP.clear();
+	EB2_det_tlP.clear();
+	PB2_det_tlP.clear(); //Calculated heavy particle momentum
+	fEYd2_tlP.clear();
+	fCCsI2_tlP.clear();
+	fECsI2_tlP.clear();
+	fEb2_tlP.clear();
+	fPb2_tlP.clear();
+  	fPby2_tlP.clear();
+  	fPbxcm2_tlP.clear();
+	fThetacm2_tlP.clear();
+	fTheta2_tlP.clear();
+
+	fPBeam_tlP.clear();
+  	fPResid_tlP.clear();
+  	fA_tlP.clear();
+  	fB_tlP.clear();
+  	fC_tlP.clear();
+	fEB_tlP.clear();
+	TSdThetaCM_tlP.clear();
+	TSdETot_tlP.clear();
+
+	Qdet1_blP.clear();
+	Ex1_blP.clear();
+	YdCsI1ETot_blP.clear();
+	EB1_det_blP.clear(); //Calculated heavy particle energy
+	PB1_det_blP.clear(); //Calculated heavy particle momentum
+	fEYd1_blP.clear();
+	fCCsI1_blP.clear(); 
+	fECsI1_blP.clear(); 
+	fEb1_blP.clear();
+  	fPb1_blP.clear();
+  	fPby1_blP.clear();
+  	fPbxcm1_blP.clear();
+	fThetacm1_blP.clear();
+	fTheta1_blP.clear();
+	
+	Qdet2_blP.clear();
+	Ex2_blP.clear();
+	YdCsI2ETot_blP.clear();
+	EB2_det_blP.clear(); //Calculated heavy particle energy
+	PB2_det_blP.clear(); //Calculated heavy particle momentum
+	fEYd2_blP.clear();
+	fCCsI2_blP.clear(); 
+	fECsI2_blP.clear(); 
+	fEb2_blP.clear();
+  	fPb2_blP.clear();
+  	fPby2_blP.clear();
+  	fPbxcm2_blP.clear();
+	fThetacm2_blP.clear();
+	fTheta2_blP.clear();
+
+	fPBeam_blP.clear();
+  	fPResid_blP.clear();
+  	fA_blP.clear();
+  	fB_blP.clear();
+  	fC_blP.clear();
+	fEB_blP.clear();
+	TSdThetaCM_blP.clear();
+	TSdETot_blP.clear();
+
+	//Upstream
+
+	fEbU = sqrt(-1.);
+  	fPbU = sqrt(-1.);
+  	fPbUy = sqrt(-1.);
+  	fPbUxcm = sqrt(-1.);
+	fEBU=0; //Calculated heavy particle energy
+	fPBU=0; //Calculated heavy particle momentum
+	fQvU=0;
+	fExU = 0 ;
+	fThetaDU=0;
+	fThetacmU=0;
+	
+	fEbUSd = sqrt(-1.);
+  	fPbUSd = sqrt(-1.);
+  	fPbUSdy = sqrt(-1.);
+  	fPbUSdxcm = sqrt(-1.);
+	fEBUSd=0; //Calculated heavy particle energy
+	fPBUSd=0; //Calculated heavy particle momentum
+	fQvUSd=0;
+	fThetacmUSd=0;
+	fThetaDUSd=0;
+
+
+	
 	
 }
 
